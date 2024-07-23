b1763837bd5a5693e209b2690e34ac4a7c602b5f
Ravisharma-1999 <ravi829144@gmail.com>
2024-07-23 22:24:40 +0530

    created a submission

--- script.js
+++
@@ -1,7 +0,0 @@
-//your JS code here. If required.
-it('should have required elements', () => {
-  const tags = ['header', 'div', 'h3', 'ul', 'li', 'h2', 'p', 'img'];
-  tags.forEach(tag => {
-    cy.get(tag, { timeout: 10000 }).should('exist');
-  });
-});
